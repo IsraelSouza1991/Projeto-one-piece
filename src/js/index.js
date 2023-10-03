dc55dcb19b0a138fdefa20f3125e51c5d8d9ef6f
IsraelSouza <israel.felipe1991@gmail.com>
2023-10-03 03:00:08 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -2,13 +2,21 @@ const botoes = document.querySelectorAll('.botao');
 const personagens = document.querySelectorAll(".personagem");
 
 botoes.forEach((botao, indice) => {
-    botao.addEventListener("click", () =>{
-        const botaoSelecionado = document.querySelector(".botao.selecionado");
-        botaoSelecionado.classList.remove("selecionado");
-        
+    botao.addEventListener("click", () => {
+
+        desselecionarBotao()
+        desselecionarPersonagem();
+
         botao.classList.add("selecionado");
-        const personagemSelecionado = document.querySelector(".personagem.selecionado");
-        personagemSelecionado.classList.remove("selecionado");
         personagens[indice].classList.add("selecionado");
     })
-})
+});
+
+function desselecionarBotao() {
+    const botaoSelecionado = document.querySelector(".botao.selecionado");
+    botaoSelecionado.classList.remove("selecionado");
+}
+function desselecionarPersonagem(){
+    const personagemSelecionado = document.querySelector(".personagem.selecionado");
+    personagemSelecionado.classList.remove("selecionado");
+}
