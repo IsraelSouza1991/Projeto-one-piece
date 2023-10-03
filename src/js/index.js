c3193c150fea655f38deb91fde88780e79421fd5
IsraelSouza <israel.felipe1991@gmail.com>
2023-10-03 02:49:41 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,7 @@ botoes.forEach((botao, indice) => {
     botao.addEventListener("click", () =>{
         const botaoSelecionado = document.querySelector(".botao.selecionado");
         botaoSelecionado.classList.remove("selecionado");
+        
         botao.classList.add("selecionado");
         const personagemSelecionado = document.querySelector(".personagem.selecionado");
         personagemSelecionado.classList.remove("selecionado");
